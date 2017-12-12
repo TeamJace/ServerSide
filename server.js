89eb52cf617fb19a2a71193755afd10eefbb2e34
Jack <lomax15@me.com>
2017-12-12 15:16:18 -0800

    added book.find to Book

--- server.js
+++ server.js
@@ -25,18 +25,6 @@ app.get('/api/v1/books', (req, res) => {
         .then(data => res.send(data.rows)).catch(console.error);
 });
 
-app.get('/api/v1/books/:id', (req, res) => {
-    client.query('SELECT * FROM books WHERE id = $1', [req.params.id])
-        .then(data => res.send(data.rows)).catch(console.error);
-});
-
-app.post('/api/v1/new', (req, res) => {
-    client.query('INSERT INTO books (title, author, isbn, "image_url", description) VALUES ($1, $2, $3, $4, $5)', [req.body.title, req.body.author, req.body.isbn, req.body.image_url, req.body.descrption])
-        .then(console.log)
-        .then(data => res.send(data))
-        .catch(console.error);
-});
-
 app.get('api/v1/books/search', (req, res) => {
     const query = req.query.find;
     superagent
@@ -55,6 +43,20 @@ app.get('api/v1/books/search', (req, res) => {
         })
         .catch(console.error);
 });
+
+app.get('/api/v1/books/:id', (req, res) => {
+    client.query('SELECT * FROM books WHERE id = $1', [req.params.id])
+        .then(data => res.send(data.rows)).catch(console.error);
+});
+
+app.post('/api/v1/new', (req, res) => {
+    client.query('INSERT INTO books (title, author, isbn, "image_url", description) VALUES ($1, $2, $3, $4, $5)', [req.body.title, req.body.author, req.body.isbn, req.body.image_url, req.body.descrption])
+        .then(console.log)
+        .then(data => res.send(data))
+        .catch(console.error);
+});
+
+
 
 app.get('*', (req, res) => {
     res.send('ERROR path does not exist');
